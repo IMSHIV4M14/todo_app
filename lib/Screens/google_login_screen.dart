71ac9080a679ac8a8f353d6ba6512e875a2577a3
Shivam Mandal <mandalshiva444@gmail.com>
2026-07-02 17:49:25 +0545

    generating berear token

---
+++ lib/Screens/google_login_screen.dart
@@ -0,0 +1,69 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class GoogleLoginScreen extends StatefulWidget {
+  const GoogleLoginScreen({super.key});
+
+  @override
+  State<GoogleLoginScreen> createState() => _GoogleLoginScreenState();
+}
+
+class _GoogleLoginScreenState extends State<GoogleLoginScreen> {
+  String token = 'lodaing ...';
+
+  // @override
+  // void initState() {
+  //   getFCMToken();
+  //   super.initState();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Google Login Screen'), centerTitle: true),
+      body: Column(
+        crossAxisAlignment: .center,
+        spacing: 50,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              getFCMToken();
+            },
+            child: Text('Generate FCM Token'),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text('FCM Token is : "$token"'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // get FCM token
+  Future<void> getFCMToken() async {
+    // requestPermission to user
+    await FirebaseMessaging.instance.requestPermission();
+
+    // get FCM token from firebase messaging
+
+    String? t = await FirebaseMessaging.instance.getToken();
+    setState(() {
+      token = t ?? 'No Token Found 🥲';
+    });
+
+    print('Fcm Token is : "$token"');
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      if (kDebugMode) {
+        print('Foreground: ${message.notification?.title}');
+      }
+    });
+
+    RemoteMessage? initialMessage = await FirebaseMessaging.instance
+        .getInitialMessage();
+    if (initialMessage != null) {
+      print('From Terminated Stste');
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app/firebase_options.dart';
 import 'package:todo_app/helper/local_notification_service.dart';
 import 'package:todo_app/my_app.dart';
 
+// step 1
+@pragma('vm:entry-point')
+Future<void> _backgroundHandler(RemoteMessage message) async {
+  if (kDebugMode) {
+    print('Message is : ${message.notification?.title}');
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Must be first!
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // step 2 register and initilized
+  FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
+
   await initLocalNotification();
 
   runApp(const MyApp());
